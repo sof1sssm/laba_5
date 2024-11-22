847be6a85a2c7b434f14d80b36addd82fbe6b609
sof1sssm <125864802+sof1sssm@users.noreply.github.com>
2024-11-23 01:11:47 +0700

    Update users.py

--- users.py
+++ users.py
@@ -1,11 +1,11 @@
-from flask_login import UserMixin
+from flask_login import UserMixin #класс с методами для аунтификации пользователей
 
-class User(UserMixin):
+class User(UserMixin): #класс User будет иметь все функциональные возможности, предоставляемые UserMixin
     def __init__(self, id, email, password, name):
-        self.id = id
-        self.email = email
-        self.password = password
-        self.name = name
+        self.id = id #присваивает значение id атрибуту id экземпляра пользователя
+        self.email = email #присваивает значение email атрибуту email
+        self.password = password # присваивает значение password атрибуту password
+        self.name = name # присваивает значение name атрибуту name
 
-# Заглушка для хранения пользователей
+# Заглушка для хранения пользователей (словарь)
 users_db = {} 
